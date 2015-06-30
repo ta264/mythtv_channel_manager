40e8b40921696a6850bcb4441a0cce471beb726d
ta264 <tom@andrewsmail.co.uk>
2015-06-30 10:40:00 +0100

    Add SQL to dump existing transponders being used

---
+++ contrib/transponders.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT SUBSTRING(d.mod_sys, 5), d.frequency, UPPER(d.polarity), d.symbolrate, d.fec, d.rolloff * 100, UPPER(d.constellation) FROM dtv_multiplex AS d INNER JOIN channel ON d.mplexid = channel.mplexid ORDER BY d.frequency INTO OUTFILE '/tmp/Astra_freesat' FIELDS TERMINATED BY ' ' LINES TERMINATED BY '\n';
